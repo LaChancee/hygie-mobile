bf051e543b8a498108776ea4d7a6026a53543130
guillaumetroyaux <guillaumetroyaux@hotmail.fr>
2024-09-27 13:47:44 +0200

    organisation projet

---
+++ lib/presentation/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:hygie_mobile/presentation/recompense/recompense_view.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  // Liste des widgets correspondant à chaque écran
+  final List<Widget> _screens = [
+    // DashboardScreen(),
+    // JournalScreen(),
+    RecompenseView()
+    // ObjectifsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mon Application'),
+      ),
+      body: _screens[_selectedIndex],  // Affiche l'écran sélectionné
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Journal',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Récompenses',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flag),
+            label: 'Objectifs',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/presentation/recompense/recompense_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecompenseView extends StatefulWidget {
+  const RecompenseView({super.key});
+
+  @override
+  State<RecompenseView> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<RecompenseView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
