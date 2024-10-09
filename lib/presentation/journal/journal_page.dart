9d109799bc454812d170564236aaf61b9177ba15
guillaumetroyaux <guillaumetroyaux@hotmail.fr>
2024-10-09 11:51:08 +0200

    modification du design des page journal et objectif et progression

--- lib/presentation/journal/journal_page.dart
+++ lib/presentation/journal/journal_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:hygie_mobile/commons/header.dart';
-import 'package:intl/intl.dart'; // Assurez-vous d'avoir importé ce package dans votre pubspec.yaml
 
 class JournalPage extends StatefulWidget {
   const JournalPage({Key? key}) : super(key: key);
@@ -9,102 +9,162 @@ class JournalPage extends StatefulWidget {
   _JournalPageState createState() => _JournalPageState();
 }
 
-class _JournalPageState extends State<JournalPage> {
-  DateTime selectedDate = DateTime.now(); // Date sélectionnée, initialisée à aujourd'hui
+class _JournalPageState extends State<JournalPage> with SingleTickerProviderStateMixin {
+  DateTime selectedDate = DateTime.now();
+  late TabController _tabController;
 
-final Map<DateTime, List<Map<String, String>>> activities = {
-  DateTime.now(): [
-    {'time': '12:05', 'description': '2 cigarettes consommées'},
-    {'time': '10:23', 'description': '1 cigarette consommée'},
-    {'time': '08:45', 'description': '1 cigarette consommée'},
-  ],
-  DateTime(2024, 10, 9): [ // Exemples pour le 7 octobre 2024
-    {'time': '09:30', 'description': '2 cigarette consommée'},
-    {'time': '10:23', 'description': '1 cigarette consommée'},
-    {'time': '08:45', 'description': '1 cigarette consommée'},
-  ],
-};
+  final Map<DateTime, List<Map<String, String>>> activities = {
+    DateTime.now(): [
+      {'time': '12:05', 'description': '2 cigarettes consommées'},
+      {'time': '10:23', 'description': '1 cigarette consommée'},
+    ],
+    DateTime(2024, 10, 9): [
+      {'time': '09:30', 'description': '2 cigarettes consommées'},
+      {'time': '10:23', 'description': '1 cigarette consommée'},
+    ],
+  };
 
-  // Méthode pour changer la date
-  void _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: selectedDate,
-      firstDate: DateTime(2022), // Date minimum
-      lastDate: DateTime.now(), // Date maximum (aujourd'hui)
-    );
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked; // Mettre à jour la date sélectionnée
-      });
-    }
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  void _incrementDate(int days) {
+    setState(() {
+      selectedDate = selectedDate.add(Duration(days: days));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Utilisation du header réutilisable
-            const Header(title: "",),
-            
-            // Section de sélection de date
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Date: ${DateFormat.yMMMMd().format(selectedDate)}',
-                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => _selectDate(context),
-                    child: const Text('Changer de date'),
-                  ),
-                ],
-              ),
-            ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Header(title: ""),
+          SizedBox(height: 20),
 
-            // Affichage des activités du jour
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Activités enregistrées :',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 10),
-                  ..._buildActivityList(selectedDate),
-                ],
+          // Onglets "Activités" et "Programme"
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TabBar(
+              controller: _tabController,
+              labelColor: Colors.white, // Couleur du texte sélectionné
+              unselectedLabelColor: Colors.black54,
+              indicator: BoxDecoration(
+                color: Color.fromRGBO(4, 75, 217, 1), // Fond bleu pour l'onglet actif
+                borderRadius: BorderRadius.circular(8), // Coins arrondis
               ),
+              tabs: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.5, // Largeur personnalisée
+                  child: Tab(
+                    text: "Activités",
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.5, // Largeur personnalisée
+                  child: Tab(
+                    text: "Programme",
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20),
+
+          // Affichage de la date choisie (sans carte)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.chevron_left, color: Colors.blue),
+                  onPressed: () => _incrementDate(-1),
+                ),
+                // Texte de la date avec style de couleur
+                Text(
+                  DateFormat('EEEE d MMMM').format(selectedDate),
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromRGBO(4, 75, 217, 1)), // Couleur bleue
+                ),
+                IconButton(
+                  icon: const Icon(Icons.chevron_right, color: Colors.blue),
+                  onPressed: () => _incrementDate(1),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+
+          // Contenu des onglets
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                // Vue "Activités"
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: _buildActivityList(selectedDate),
+                ),
+                // Vue "Programme"
+                const Center(child: Text('Programme - En construction')),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  // Méthode pour construire la liste des activités selon la date sélectionnée
-  List<Widget> _buildActivityList(DateTime date) {
-    // Normaliser la date à uniquement l'année, le mois et le jour
+  // Méthode pour construire la liste des activités du jour
+  Widget _buildActivityList(DateTime date) {
     final formattedDate = DateTime(date.year, date.month, date.day);
     final dailyActivities = activities[formattedDate] ?? [];
 
     if (dailyActivities.isEmpty) {
-      return [const Text('Aucune activité enregistrée pour cette date.')];
+      return const Center(child: Text('Aucune activité enregistrée pour cette date.'));
     }
 
-    return dailyActivities.map((activity) {
-      return ListTile(
-        leading: const Icon(Icons.check_circle, color: Colors.green),
-        title: Text('${activity['time']}'),
-        subtitle: Text(activity['description']!),
-      );
-    }).toList();
+    return ListView.builder(
+      itemCount: dailyActivities.length,
+      itemBuilder: (context, index) {
+        final activity = dailyActivities[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0),
+          child: Row(
+            children: [
+              // Icône circulaire pour l'activité
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.grey[200],
+                child: const Icon(Icons.local_fire_department, color: Colors.grey),
+              ),
+              SizedBox(width: 10),
+
+              // Détails de l'activité
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      activity['description']!,
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Heure de l'activité
+              Text(
+                activity['time']!,
+                style: const TextStyle(fontSize: 16, color: Colors.black54),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
--- lib/presentation/objectifs/objectif_page.dart
+++ lib/presentation/objectifs/objectif_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hygie_mobile/commons/header.dart';
-
 import 'progression_page';
 
 class ObjectifsPage extends StatefulWidget {
@@ -9,7 +8,6 @@ class ObjectifsPage extends StatefulWidget {
 }
 
 class _ObjectifsPageState extends State<ObjectifsPage> {
-  // Variables pour gérer l'affichage des sous-menus
   bool showHealthGoals = false;
   bool showSavingsGoals = false;
 
@@ -21,12 +19,13 @@ class _ObjectifsPageState extends State<ObjectifsPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Header
-            Header(title: "",),
+            Header(title: ""),
             SizedBox(height: 20),
 
             // Rubrique Objectifs de santé
-            _buildExpandableTile(
-              title: 'Objectifs de santé',
+            _buildGoalSection(
+              title: 'Objectif de santé',
+              icon: Icons.local_fire_department,
               expanded: showHealthGoals,
               onTap: () {
                 setState(() {
@@ -35,16 +34,19 @@ class _ObjectifsPageState extends State<ObjectifsPage> {
               },
               subOptions: showHealthGoals
                   ? [
-                      _buildSubOption(context, 'Alcool', 'Alcool'),
-                      _buildSubOption(context, 'Tabac', 'Tabac'),
+                      _buildSubOption(context, 'Tabac', 'Objectifs validés 2/46', 'Tabac', Colors.blue[50]!),
+                      SizedBox(height: 10), // Espace entre Tabac et Alcool
+                      _buildSubOption(context, 'Alcool', 'Objectifs validés 1/10', 'Alcool', Colors.blue[50]!),
+                      SizedBox(height: 20), // Espacement ajouté en dessous de la carte Alcool
                     ]
                   : [],
             ),
             SizedBox(height: 20),
 
-            // Rubrique Objectifs d'épargne
-            _buildExpandableTile(
-              title: 'Objectifs d\'épargne',
+            // Rubrique Objectifs d'épargne avec les mêmes espacements
+            _buildGoalSection(
+              title: 'Objectif d\'épargne',
+              icon: Icons.savings,
               expanded: showSavingsGoals,
               onTap: () {
                 setState(() {
@@ -53,72 +55,117 @@ class _ObjectifsPageState extends State<ObjectifsPage> {
               },
               subOptions: showSavingsGoals
                   ? [
-                      _buildSubOption(context, 'Alcool', 'Alcool'),
-                      _buildSubOption(context, 'Tabac', 'Tabac'),
+                      _buildSubOption(context, 'Tabac', 'Objectifs validés 2/46', 'Tabac', Colors.blue[50]!),
+                      SizedBox(height: 10), // Espace entre Tabac et Alcool
+                      _buildSubOption(context, 'Alcool', 'Objectifs validés 1/10', 'Alcool', Colors.blue[50]!),
+                      SizedBox(height: 20), // Espacement ajouté en dessous de la carte Alcool pour épargne
                     ]
                   : [],
             ),
             SizedBox(height: 20),
 
             // Rubrique Se fixer un objectif
-            _buildSimpleTile('Se fixer un objectif', Icons.add, () {
-              // Action pour créer un objectif personnalisé
-            }),
+            _buildSimpleTile('Se fixer un objectif', Icons.add),
           ],
         ),
       ),
     );
   }
 
-  // Widget pour les rubriques dépliables
-  Widget _buildExpandableTile({
+  // Widget pour les rubriques d'objectif avec icône et options
+  Widget _buildGoalSection({
     required String title,
+    required IconData icon,
     required bool expanded,
     required VoidCallback onTap,
     required List<Widget> subOptions,
   }) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ListTile(
-          title: Text(
-            title,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          trailing: Icon(expanded ? Icons.expand_less : Icons.expand_more),
-          onTap: onTap,
-        ),
-        if (expanded) ...subOptions,
-      ],
-    );
-  }
-
-  // Widget pour les sous-options (Alcool, Tabac)
-  Widget _buildSubOption(BuildContext context, String label, String type) {
     return Padding(
-      padding: const EdgeInsets.only(left: 16.0),
-      child: ListTile(
-        title: Text(label),
-        onTap: () {
-          // Naviguer vers une page de progression pour Alcool ou Tabac
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ProgressionPage(type: type)),
-          );
-        },
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 0, // Suppression de l'ancienne élévation
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2), // Ombrage léger et discret
+                spreadRadius: 2,
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(icon, color: Color.fromRGBO(4, 75, 217, 1)),
+                title: Text(
+                  title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                trailing: Icon(expanded ? Icons.expand_less : Icons.expand_more),
+                onTap: onTap,
+              ),
+              if (expanded) ...subOptions,
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  // Widget pour une simple rubrique (comme "Se fixer un objectif")
-  Widget _buildSimpleTile(String title, IconData icon, VoidCallback onTap) {
-    return ListTile(
-      title: Text(
-        title,
-        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+  // Widget pour les sous-options (Tabac, Alcool) sans ombrage
+  Widget _buildSubOption(BuildContext context, String label, String subtitle, String type, Color backgroundColor) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        color: backgroundColor, // Couleur de fond uniforme
+        elevation: 0, // Suppression de l'ombrage pour les sous-options
+        child: ListTile(
+          title: Text(label),
+          subtitle: Text(subtitle),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProgressionPage(type: type)),
+            );
+          },
+        ),
       ),
-      trailing: Icon(icon),
-      onTap: onTap,
+    );
+  }
+
+  // Widget pour une simple rubrique (comme "Se fixer un objectif") avec ombrage discret
+  Widget _buildSimpleTile(String title, IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 0, // Suppression de l'ancienne élévation
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2), // Ombrage léger
+                spreadRadius: 2,
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: ListTile(
+            leading: Icon(icon, color: Colors.black),
+            title: Text(
+              title,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
