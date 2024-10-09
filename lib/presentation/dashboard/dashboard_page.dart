12634878ff31e2f40d210cf4464f2d5764ea849e
LaChancee <miharisoababef@gmail.com>
2024-10-09 16:41:38 +0200

    Refactor code for discover_button.dart

--- lib/presentation/dashboard/dashboard_page.dart
+++ lib/presentation/dashboard/dashboard_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:hygie_mobile/commons/colors.dart';
 import 'package:hygie_mobile/commons/header.dart';
 import 'package:hygie_mobile/presentation/dashboard/header_dashboard.dart'; // Assurez-vous que le chemin est correct
 import 'package:hygie_mobile/presentation/dashboard/test_button.dart'; // Assurez-vous que le chemin est correct
 import 'package:hygie_mobile/presentation/dashboard/widget_dashboard.dart'; // Assurez-vous que le chemin est correct
-
-
+import 'package:hygie_mobile/presentation/dashboard/discover_button.dart'; // Assurez-vous que le chemin est correct
 
 class DashboardPage extends StatelessWidget {
   @override
@@ -16,10 +14,15 @@ class DashboardPage extends StatelessWidget {
         slivers: [
           // SliverAppBar, qui se rétrécit au scroll
           SliverAppBar(
-            title: Header(title: '',),
-            expandedHeight: 300.0, // Hauteur de l'expansion
-            floating: false, // Le header ne flotte pas au-dessus
-            pinned: true, // Le header reste visible quand on scrolle
+            title: Header(
+              title: '',
+            ),
+            expandedHeight: 300.0,
+            // Hauteur de l'expansion
+            floating: false,
+            // Le header ne flotte pas au-dessus
+            pinned: true,
+            // Le header reste visible quand on scrolle
             flexibleSpace: FlexibleSpaceBar(
               background: HeaderDashboard(),
               centerTitle: true,
@@ -58,46 +61,11 @@ class DashboardPage extends StatelessWidget {
                     WidgetsSection(),
                     SizedBox(height: 20),
                     // Section des Offres Personnalisées
-                    _buildOfferSection(),
+                    DiscoverButton(),
                   ],
                 ),
               ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-
-
-// Custom offer section
-  Widget _buildOfferSection() {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: Color(0xFFE6F5FF),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Découvrez notre offre Hygie+ !',
-            style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF333333)),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Accédez à des fonctionnalités supplémentaires, des programmes de soutien exclusifs, et bien plus encore. Abonnez-vous dès maintenant pour profiter de tous les avantages !',
-            style: TextStyle(fontSize: 14, color: Color(0xFF777777)),
-          ),
-          SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Je découvre'),
           ),
         ],
       ),
